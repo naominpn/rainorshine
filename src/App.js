a523e8aa47b0aa8ae9b0149cbd37c1f7607612a1
Naomi <naomi.np.nguyen@gmail.com>
2021-08-06 05:31:03 +1000

    Creating context, useContext, provider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import { useState } from 'react'
+import { useContext } from 'react'
+import { WeatherContext } from './WeatherContext';
+
 import './App.css';
 // import LocationSelect from './LocationSelect.js'
 // import CurrrentWeather from './CurrentWeather.js';
-import axios from 'axios'
-import data from './data.js'
+
 
 function App() {
-  const [cityWeatherForecast, setCityWeatherForecast] = useState([])
-
-  const handleChange = e => {
-    var location = e.target.value;
-    if (!!location) {
-      axios
-          .get(`https://api.openweathermap.org/data/2.5/onecall?lat=${data[location].lat}&lon=${data[location].lon}&exclude=hourly,minutely&appid=8404fc7b2bbd4a1a4d6a797d61b3cbb0`)
-          .then(res => {
-            console.log(res.data)
-            setCityWeatherForecast([res.data])
-          })
-    } else {
-      setCityWeatherForecast([])
-    }
-  }
+  const {
+    cityWeatherForecast, 
+    handleChange
+  } = useContext(WeatherContext)
 
   const toDate = (dt, timeZone) => {
     const options = { timeZone: timeZone, weekday: "long", year: 'numeric', month: 'long', day: 'numeric' }
@@ -31,8 +21,6 @@ function App() {
     return (tempK - 273.15).toFixed(2)
   }
   
-  
-
   return (
     <div className="App">
         {/* <Heading /> */}
@@ -69,8 +57,8 @@ function App() {
             : <div className="days-weather">
                 <h1>7 day forecast</h1>
                 
-                  {cityWeatherForecast[0].daily.slice(1).map((day, index) => 
-                    <div>
+                  {cityWeatherForecast[0].daily.slice(1).map( (day, idx) => 
+                    <div key={idx}>
                       <p>{toDate(day.dt, cityWeatherForecast[0].timezone)}</p>
                       <p>{day.weather[0].main}</p>                  
                       <p>{toCelcius(day.temp.day)}°C</p>
@@ -80,8 +68,6 @@ function App() {
               </div> 
           }
         </div>
-
-        {/* <CurrrentWeather /> */}
 
     </div>
   );
---
+++ src/WeatherContext.js
@@ -0,0 +1,27 @@
+import React, {useState, createContext} from "react";
+import axios from 'axios'
+import data from './data.js'
+export const WeatherContext = createContext();
+
+
+export function WeatherProvider(props) {
+    const [cityWeatherForecast, setCityWeatherForecast] = useState([])
+
+    const handleChange = e => {
+        var location = e.target.value;
+        if (!!location) {
+          axios
+              .get(`https://api.openweathermap.org/data/2.5/onecall?lat=${data[location].lat}&lon=${data[location].lon}&exclude=hourly,minutely&appid=8404fc7b2bbd4a1a4d6a797d61b3cbb0`)
+              .then(res => {
+                console.log(res.data)
+                setCityWeatherForecast([res.data])
+              })
+        } else {
+          setCityWeatherForecast([])
+        }
+      }
+
+    return <WeatherContext.Provider value={{cityWeatherForecast, handleChange}}>
+        {props.children}
+    </WeatherContext.Provider>
+}
