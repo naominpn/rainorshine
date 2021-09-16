09538f79943ff4ee74a9615fa0a81cfa9d0ebff4
Naomi <naomi.np.nguyen@gmail.com>
2021-09-16 23:00:25 +1000

    made variables and improved readability

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -5,62 +5,64 @@ import './CurrentWeather.css'
 
 export default function CurrrentWeather() {
     const { cityWeatherForecast } = useContext(WeatherContext)
-   return( 
-   <div className="current-weather">
-       {
-       cityWeatherForecast.length === 0
-       ? <p></p> //loading component
-       : <section className="current-section"> 
+    if (cityWeatherForecast.length === 0) {
+        return <div className="current-weather">
+                <p></p>
+        </div>;
+    }
+
+    var forecast = cityWeatherForecast[0];
+    var current =  forecast.current;
+    var timezone = forecast.timezone;
+    var daily = forecast.daily[0];
+    var weather = current.weather[0];
+
+    return( 
+    <div className="current-weather">
+        <section className="current-section"> 
        {/* make into seperate component */}
                 <div className="current-leftSide">
                     <p className="heading-date">
-                        {getDay(cityWeatherForecast[0].current.dt, cityWeatherForecast[0].timezone)}, {toDate(cityWeatherForecast[0].current.dt, cityWeatherForecast[0].timezone)}
+                        {getDay(current.dt, timezone)}, {toDate(current.dt, timezone)}
                     </p>
                     
                     <span className="span-now">Now:</span>
 
                     <div className="current-info">
                         <div className="icon-div">
-                            <img className="current-icon" src={`http://openweathermap.org/img/wn/${cityWeatherForecast[0].current.weather[0].icon}.png`} alt="" />
+                            <img className="current-icon" src={`http://openweathermap.org/img/wn/${weather.icon}.png`} alt="" />
                         </div>
                         <div>
-                            <p>{cityWeatherForecast[0].current.weather[0].main}</p>
-                            <p>{toCelcius(cityWeatherForecast[0].current.temp)} °C</p>
+                            <p>{weather.main}</p>
+                            <p>{toCelcius(current.temp)} °C</p>
                         </div>
                     </div>
                 </div>
 
 
                 <div className="current-rightSide">
-                    <p className="today-info">{toCelcius(cityWeatherForecast[0].daily[0].temp.min)}°C 
+                    <p className="today-info">{toCelcius(daily.temp.min)}°C 
                         <span className="label">High</span>
                     </p>
-                    <p className="today-info">{cityWeatherForecast[0].daily[0].wind_speed}m/s 
+                    <p className="today-info">{daily.wind_speed} m/s 
                         <span className="label">Wind</span>
                     </p>
                     <p className="today-info">
-                        {
-                            toTime(cityWeatherForecast[0].daily[0].sunrise, 
-                            cityWeatherForecast[0].timezone)
-                        } 
-                         <span className="label"> Sunrise</span>
+                        {toTime(daily.sunrise, timezone)} 
+                        <span className="label"> Sunrise</span>
                     </p>
-                    <p className="today-info">{toCelcius(cityWeatherForecast[0].daily[0].temp.max)}°C 
+                    <p className="today-info">{toCelcius(daily.temp.max)}°C 
                         <span className="label"> Low </span>
                     </p>
-                    <p className="today-info">{Math.round(cityWeatherForecast[0].daily[0].pop*100)}% 
+                    <p className="today-info">{Math.round(daily.pop*100)}% 
                         <span className="label"> Rain </span>
                     </p>
                     <p className="today-info">
-                        {
-                            toTime(cityWeatherForecast[0].daily[0].sunset, 
-                            cityWeatherForecast[0].timezone)
-                        } 
-                         <span className="label">Sunset</span>
+                        {toTime(daily.sunset, timezone)} 
+                        <span className="label">Sunset</span>
                     </p>
                 </div>
         </section> 
-        }
     </div>
-   )
+    )
 } 
--- src/components/DayWeather.js
+++ src/components/DayWeather.js
@@ -3,13 +3,11 @@ import { WeatherContext} from './../WeatherContext';
 import {toCelcius, toDate, getDay} from './../conversionFunctions'
 import './DayWeather.css'
 
-
-
 export default function DayWeather(props) {
     const { cityWeatherForecast } = useContext(WeatherContext)
     const icon = `http://openweathermap.org/img/wn/${props.day.weather[0].icon}.png`
 
-   return( 
+    return( 
         <div className="day-weather" key={props.idx}>
             <p className="theDay">{getDay(props.day.dt, cityWeatherForecast[0].timezone)}</p>
             <p>{toDate(props.day.dt, cityWeatherForecast[0].timezone)}</p>
@@ -19,3 +17,5 @@ export default function DayWeather(props) {
         </div>
     )
 }
+
+
